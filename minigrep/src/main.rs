a146eb6538deede62469a7fb8051d5d1ca8ed75d
code-cp <moshan.nus@gmail.com>
2023-07-30 13:06:25 +0800

    update code

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,13 @@
 use std::env; 
+use std::fs; 
 
 fn main() {
     let args: Vec<String> = env::args().collect(); 
-    dbg!(args); 
+    // dbg!(args); 
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("searching for {} in {}", query, file_path); 
+
+    let contents = fs::read_to_string(file_path).expect("should be able to read the file");
+    println!("with text\n{contents}");
 }
