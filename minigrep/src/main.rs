b419be720fe730be5ca4be2065e17ec4ffce1034
code-cp <moshan.nus@gmail.com>
2023-07-30 13:15:34 +0800

    refactor

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,11 +3,23 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); 
-    // dbg!(args); 
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("searching for {} in {}", query, file_path); 
+    let config = parse_config(&args);
+    println!("searching for {} in {}", config.query, config.file_path); 
 
-    let contents = fs::read_to_string(file_path).expect("should be able to read the file");
+    let contents = fs::read_to_string(config.file_path).expect("should be able to read the file");
     println!("with text\n{contents}");
 }
+
+struct Config {
+    query: String, 
+    file_path: String, 
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    
+    Config {query, file_path}
+}
+
+
